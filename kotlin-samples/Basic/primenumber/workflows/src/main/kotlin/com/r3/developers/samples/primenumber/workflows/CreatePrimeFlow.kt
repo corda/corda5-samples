d45fa5fb3160656cd42d9ae7762c055b6ef96811
Christian Napoles <christian.napoles@r3.com>
2023-08-03 14:42:35 +0100

    CORE-15575: cleaned up prime number sample

--- kotlin-samples/Basic/primenumber/workflows/src/main/kotlin/com/r3/developers/samples/primenumber/workflows/CreatePrimeFlow.kt
+++ kotlin-samples/Basic/primenumber/workflows/src/main/kotlin/com/r3/developers/samples/primenumber/workflows/CreatePrimeFlow.kt
@@ -1,4 +1,4 @@
-package com.r3.developers.samples.primenumber.flows
+package com.r3.developers.samples.primenumber.workflows
 
 import com.r3.developers.samples.primenumber.Prime
 import com.r3.developers.samples.primenumber.PrimeCommands
@@ -35,18 +35,16 @@ class CreatePrime(): ClientStartableFlow {
     private companion object {
         val log = LoggerFactory.getLogger(this::class.java.enclosingClass)
 
-        const val CALLED = "oracle-primenumber: CreatePrimeFlow.call() called"
+        const val CALLED = "CreatePrimeFlow.call() called"
         const val SET_UP = "Initialising flows."
-        const val QUERYING_THE_ORACLE = "Querying oracle for the Nth prime."
+        const val QUERYING_THE_PRIME_SERVICE = "Querying prime service for the Nth prime."
         const val BUILDING_THE_TX = "Building transaction."
         const val VERIFYING_THE_TX = "Verifying transaction."
         const val WE_SIGN = "signing transaction."
         const val FINALISING = "Finalising transaction."
 
-
-        //HARDCODED Notary and Oracle MemberX500Name - can be configured
         val notaryName: MemberX500Name = MemberX500Name.parse("CN=NotaryService, OU=Test Dept, O=R3, L=London, C=GB")
-        val oracleName: MemberX500Name = MemberX500Name.parse("CN=Oracle, OU=Test Dept, O=R3, L=London, C=GB")
+        val primeServiceName: MemberX500Name = MemberX500Name.parse("CN=PrimeService, OU=Test Dept, O=R3, L=London, C=GB")
     }
 
     @CordaInject
@@ -71,18 +69,18 @@ class CreatePrime(): ClientStartableFlow {
         log.info(SET_UP)
         val request = requestBody.getRequestBodyAs(jsonMarshallingService,CreatePrimeRequest::class.java)
         val index = request.index
-        val notary = notaryLookup.lookup(notaryName) ?: throw IllegalArgumentException("Requested oracle '$notaryName' not found on network.") //Alternative: notaryLookup.notaryServices.first()
-        val oracle = memberLookup.lookup(oracleName) ?: throw IllegalArgumentException("Requested oracle '$oracleName' not found on network.")
+        val notary = notaryLookup.lookup(notaryName) ?: throw IllegalArgumentException("Requested notary '$notaryName' not found on network.") //Alternative: notaryLookup.notaryServices.first()
+        val primeService = memberLookup.lookup(primeServiceName) ?: throw IllegalArgumentException("Requested service '$primeServiceName' not found on network.")
 
-        log.info(QUERYING_THE_ORACLE)
-        val nthPrimeRequestedFromOracle: Int = flowEngine.subFlow(QueryPrimeSubFlow(oracleName,index))
+        log.info(QUERYING_THE_PRIME_SERVICE)
+        val nthPrimeResult: Int = flowEngine.subFlow(QueryPrimeSubFlow(primeServiceName,index))
 
         log.info(BUILDING_THE_TX)
         val ourIdentity = memberLookup.myInfo().ledgerKeys.first()
-        val oracleIdentity = oracle.ledgerKeys.first()
-        val primeState = Prime(index, nthPrimeRequestedFromOracle,listOf(ourIdentity))
-        val primeCommandData = PrimeCommands.Create(index,nthPrimeRequestedFromOracle)
-        val primeCommandRequiredSigners = listOf(oracleIdentity, ourIdentity)
+        val primeServiceIdentity = primeService.ledgerKeys.first()
+        val primeState = Prime(index, nthPrimeResult,listOf(ourIdentity))
+        val primeCommandData = PrimeCommands.Create(index,nthPrimeResult)
+        val primeCommandRequiredSigners = listOf(primeServiceIdentity, ourIdentity)
 
         log.info(VERIFYING_THE_TX) //this happens when the contractState is fed to the .addOutputState() method
         val transaction = ledgerService.createTransactionBuilder()
@@ -95,12 +93,11 @@ class CreatePrime(): ClientStartableFlow {
         log.info(WE_SIGN)
         val signedTransaction = transaction.toSignedTransaction()
 
-        log.info(FINALISING) //log.info(ORACLE_SIGN inside the subflow)
-        val finalizationResultId: SecureHash = flowEngine.subFlow(FinalizePrimeSubFlow(signedTransaction,oracleName))
+        log.info(FINALISING)
+        val finalizationResultId: SecureHash = flowEngine.subFlow(FinalizePrimeSubFlow(signedTransaction, primeServiceName))
         val finalTransaction = ledgerService.findSignedTransaction(finalizationResultId)
 
         log.info("finalized transaction: $finalTransaction")
-
         return primeState.toString()
     }
 }
