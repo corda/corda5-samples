36a48def1cdab58773a5cb2a67cf83d66a61d63a
parisyup <66366646+parisyup@users.noreply.github.com>
2024-04-30 19:17:09 +0400

    added customJSON files

---
+++ java-samples/customJSON/contracts/src/main/java/com/r3/developers/cordapptemplate/customJSON/states/CustomChatQuery.java
@@ -0,0 +1,25 @@
+package com.r3.developers.cordapptemplate.customJSON.states;
+
+import net.corda.v5.ledger.utxo.query.VaultNamedQueryFactory;
+import net.corda.v5.ledger.utxo.query.registration.VaultNamedQueryBuilderFactory;
+import org.jetbrains.annotations.NotNull;
+
+public class CustomChatQuery implements VaultNamedQueryFactory {
+
+
+    @Override
+    public void create(@NotNull VaultNamedQueryBuilderFactory vaultNamedQueryBuilderFactory) {
+
+        vaultNamedQueryBuilderFactory.create("GET_ALL_MSG")
+                .whereJson(
+                        "WHERE visible_states.custom_representation ? 'com.r3.developers.cordapptemplate.utxoexample.states.ChatState' "
+                )
+                .register();
+
+        vaultNamedQueryBuilderFactory.create("GET_MSG_FROM")
+                .whereJson(
+                        "WHERE visible_states.custom_representation -> 'com.r3.developers.cordapptemplate.utxoexample.states.ChatState' ->> 'messageContentFrom' = :nameOfSender"
+                )
+                .register();
+    }
+}
