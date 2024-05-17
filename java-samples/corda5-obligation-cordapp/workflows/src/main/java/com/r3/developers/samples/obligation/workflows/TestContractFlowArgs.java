69f4ac8ab3ce6c7e72b097a5057448412bba00cf
Faris Alblooki <66366646+parisyup@users.noreply.github.com>
2024-05-17 19:11:50 +0400

    Added Java test contract flow

---
+++ java-samples/corda5-obligation-cordapp/workflows/src/main/java/com/r3/developers/samples/obligation/workflows/TestContractFlowArgs.java
@@ -0,0 +1,18 @@
+package com.r3.developers.samples.obligation.workflows;
+
+
+public class TestContractFlowArgs {
+    private String otherMember;
+
+    public TestContractFlowArgs() {
+    }
+
+    public TestContractFlowArgs(String otherMember) {
+        this.otherMember = otherMember;
+    }
+
+    public String getOtherMember() {
+        return otherMember;
+    }
+
+}
